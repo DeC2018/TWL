fef45eb4f6f618d21a732ab85ddca658e6a47be8
Den <emailfordenis@yandex.ru>
2023-05-09 15:44:08 +1000

    Add project files.

---
+++ Bob.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "PlayableCharacter.h"
+
+class Bob : public PlayableCharacter
+{
+public:
+	// A constructor specific to Bob
+	Bob();
+
+	// The overriden input handler for Bob
+	bool virtual handleInput();
+
+};
---
+++ Thomas.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "PlayableCharacter.h"
+
+class Thomas : public PlayableCharacter
+{
+public:
+	// A constructor specific to Thomas
+	Thomas();
+
+	// The overridden input handler for Thomas
+	bool virtual handleInput();
+
+};
